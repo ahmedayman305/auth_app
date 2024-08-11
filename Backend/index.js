2bb0922506b5cd517cc94d54b4a784f42dfdd59b
ahmedayman305 <ahmedaymankamel78@gmail.com>
2024-08-11 21:27:18 +0300

    update 0.1

--- Backend/index.js
+++ Backend/index.js
@@ -30,10 +30,10 @@ app.use("/api/auth", authRouter);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.use("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
     });
 }
 
